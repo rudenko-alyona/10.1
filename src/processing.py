1f90e83609ac7f6d13bf779d1fa07ec91eae1fd3
алёна руденко <r-alen@yandex.ru>
2026-04-02 14:53:31 +0300

    Исправлен код для работы с функцией сортировки даты

--- src/processing.py
+++ src/processing.py
@@ -23,45 +23,38 @@ pending_transactions = filter_by_state(transactions, 'PENDING')
 print(pending_transactions)
 # Вывод: [{'id': 2, 'state': 'PENDING'}]
 
-
 from datetime import datetime
 
 
-def sort_by_date(data: list, reverse: bool = True):
+def sort_by_date(data_list, descending=True):
     """
-    Сортирует список словарей по дате (ключ 'date').
+    Сортирует список словарей по ключу 'date'.
+
+    :param data_list: Список словарей (например, [{'date': '01.01.2023'}, ...])
+    :param descending: Порядок сортировки (True - убывание, False - возрастание)
+    :return: Новый отсортированный список
     """
+    # Преобразуем строковую дату в объект datetime для правильного сравнения
+    # Формат даты 'dd.mm.yyyy'
+    return sorted(data, key=lambda x: x['date'], reverse=descending)
 
-    def parse_date(item: list) -> list:
-
-        date_str = item['date']
-
-        for fmt in ('%Y-%m-%d', '%d.%m.%Y', '%m/%d/%Y', '%Y/%m/%d'):
-            try:
-                return datetime.strptime(date_str, fmt)
-            except ValueError:
-                continue
-        raise ValueError(f"Неизвестный формат даты: {date_str}")
-
-        return sorted(data, key=parse_date, reverse=reverse)
-
-
-# Пример данных
-    records = [
-        {'id': 4142, 'state': 'EXECUTED',
-         'date': '2019-07-03T18:35:29.512364'},
-        {'id': 9397, 'state': 'EXECUTED',
-         'date': '2018-06-30T02:08:58.425572'},
-        {'id': 5942, 'state': 'CANCELED',
-         'date': '2018-09-12T21:27:25.241689'},
-        {'id': 6150, 'state': 'CANCELED',
-         'date': '2018-10-14T08:21:33.419441'}
-    ]
+#Пример использования
+data = [
+    {'id': 41428829, 'state': 'EXECUTED',
+    'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED',
+     'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED',
+     'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED',
+     'date': '2018-10-14T08:21:33.419441'}
+]
 
 # Сортировка по убыванию (по умолчанию)
-    sorted_desc = sort_by_date(records)
-    print("По убыванию:", sorted_desc)
+sorted_desc = sort_by_date(data)
+print("По убыванию:", sorted_desc)
 
 # Сортировка по возрастанию
-    sorted_asc = sort_by_date(records, reverse=False)
-    print("По возрастанию:", sorted_asc)
+sorted_asc = sort_by_date(data, descending=False)
+print("По возрастанию:", sorted_asc)
+
