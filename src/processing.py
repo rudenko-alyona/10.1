e2588d003daf27f283bbae3e49d25d01c6c9f7eb
алёна руденко <r-alen@yandex.ru>
2026-04-02 17:43:41 +0300

    код поправлен в соответствии с flake8

--- src/processing.py
+++ src/processing.py
@@ -23,10 +23,8 @@ pending_transactions = filter_by_state(transactions, 'PENDING')
 print(pending_transactions)
 # Вывод: [{'id': 2, 'state': 'PENDING'}]
 
-from datetime import datetime
 
-
-def sort_by_date(data_list: list, descending: bool =True):
+def sort_by_date(data_list: list, descending: bool = True):
     """
     Сортирует список словарей по ключу 'date'.
     """
@@ -34,16 +32,17 @@ def sort_by_date(data_list: list, descending: bool =True):
     # Формат даты 'dd.mm.yyyy'
     return sorted(data, key=lambda x: x['date'], reverse=descending)
 
-#Пример использования
+
+# Пример использования
 data = [
-    {'id': 41428829, 'state': 'EXECUTED',
-    'date': '2019-07-03T18:35:29.512364'},
-    {'id': 939719570, 'state': 'EXECUTED',
-     'date': '2018-06-30T02:08:58.425572'},
-    {'id': 594226727, 'state': 'CANCELED',
-     'date': '2018-09-12T21:27:25.241689'},
-    {'id': 615064591, 'state': 'CANCELED',
-     'date': '2018-10-14T08:21:33.419441'}
+        {'id': 41428829, 'state': 'EXECUTED',
+         'date': '2019-07-03T18:35:29.512364'},
+        {'id': 939719570, 'state': 'EXECUTED',
+         'date': '2018-06-30T02:08:58.425572'},
+        {'id': 594226727, 'state': 'CANCELED',
+         'date': '2018-09-12T21:27:25.241689'},
+        {'id': 615064591, 'state': 'CANCELED',
+         'date': '2018-10-14T08:21:33.419441'}
 ]
 
 # Сортировка по убыванию (по умолчанию)
@@ -53,4 +52,3 @@ print("По убыванию:", sorted_desc)
 # Сортировка по возрастанию
 sorted_asc = sort_by_date(data, descending=False)
 print("По возрастанию:", sorted_asc)
-
